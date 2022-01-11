46541d9e62c2f7e8c5f4fbc5bbc0218699b8922a
lizhe <lizhe@52tt.com>
2022-01-11 14:18:51 +0800

    fix ：fixed #17

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_novel/home/home_page.dart';
+import 'package:flutter_novel/reader/novel_reader_list.dart';
 import 'package:get/get.dart';
 
 void main() {
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: NovelReaderListPage(),
       // home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
