1e4a8fc9cfaea68d06ddad888ca7a8e47ec5cb3f
lwlizhe <25957598+lwlizhe@users.noreply.github.com>
2022-01-28 15:47:48 +0800

    test :

--- lib/test2.dart
+++ lib/test2.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_novel/common/util.dart';
+
 /// test
 class Test2Page extends StatefulWidget {
   const Test2Page({Key? key}) : super(key: key);
