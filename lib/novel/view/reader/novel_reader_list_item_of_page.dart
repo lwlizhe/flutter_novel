b610ce96ac4762fb910a9597564803004e03a592
lwlizhe <25957598+lwlizhe@users.noreply.github.com>
2022-03-08 15:24:03 +0800

    fix : 提交的时候忘了把内容显示出来……😛

--- lib/novel/view/reader/novel_reader_list_item_of_page.dart
+++ lib/novel/view/reader/novel_reader_list_item_of_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_novel/entity/novel/entity_novel_book_info.dart';
+import 'package:flutter_novel/reader/split/content_split_util.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 /// 小说阅读器 章节page部分
 class NovelListChapterPageItem extends StatelessWidget {
@@ -22,37 +24,37 @@ class NovelListChapterPageItem extends StatelessWidget {
       height: double.infinity,
       color: colorList[pageContentConfig.currentPageIndex % 4],
       alignment: AlignmentDirectional.topCenter,
-      // child: Stack(
-      //   children: [
-      //     Text.rich(TextSpan(children: [
-      //       ...ContentSplitUtil.buildTextSpanListByPageContentConfig(
-      //           pageContentConfig),
-      //       WidgetSpan(
-      //           child: GestureDetector(
-      //         onTap: () {
-      //           Fluttertoast.showToast(msg: '加了个点击事件');
-      //         },
-      //         child: Container(
-      //           width: 350,
-      //           height: 300,
-      //           color: Colors.pink,
-      //           alignment: Alignment.center,
-      //           child: Text(
-      //             '假装这里有个评论区，或者广告区;\r\n(可以点击)',
-      //             style: TextStyle(color: Colors.white, fontSize: 20),
-      //           ),
-      //         ),
-      //       )),
-      //     ])),
-      //     Positioned(
-      //         bottom: 0,
-      //         right: 0,
-      //         child: Text(
-      //           '页码:${pageContentConfig.currentPageIndex}',
-      //           style: TextStyle(color: Colors.white, fontSize: 16),
-      //         )),
-      //   ],
-      // ),
+      child: Stack(
+        children: [
+          Text.rich(TextSpan(children: [
+            ...ContentSplitUtil.buildTextSpanListByPageContentConfig(
+                pageContentConfig),
+            WidgetSpan(
+                child: GestureDetector(
+              onTap: () {
+                Fluttertoast.showToast(msg: '加了个点击事件');
+              },
+              child: Container(
+                width: 350,
+                height: 300,
+                color: Colors.pink,
+                alignment: Alignment.center,
+                child: Text(
+                  '假装这里有个评论区，或者广告区;\r\n(可以点击)',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            )),
+          ])),
+          Positioned(
+              bottom: 0,
+              right: 0,
+              child: Text(
+                '页码:${pageContentConfig.currentPageIndex}',
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              )),
+        ],
+      ),
     );
   }
 }
